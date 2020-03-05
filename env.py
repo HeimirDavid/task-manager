375081d40dc39a84670cfcb32e0634a2e9aa2811
HeimirDavid <heimir.97@gmail.com>
2020-03-05 19:51:03 +0000

    cleanup

--- env.py
+++ env.py
@@ -1,3 +1,3 @@
 import os
 
-os.environ["MONGO_URI"] = "mongodb+srv://root:UserR00t@myfirstcluster-qce1p.mongodb.net/task_manager?retryWrites=true&w=majority"
+os.environ["MONGO_URI"] = "mongodb+srv://root:<password>@myfirstcluster-qce1p.mongodb.net/task_manager?retryWrites=true&w=majority"
